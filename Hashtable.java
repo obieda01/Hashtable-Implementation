0f5faa78fa9dfc0b94014aabd9d8dd09b5640ea0
Ahmad Obiedat <obieda01@email.franklin.edu>
2016-04-25 19:44:00 -0400

    Update Hashtable.java

--- Hashtable.java
+++ Hashtable.java
@@ -1,1 +1,18 @@
 
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+
+public class HashhMap<k,v> {
+	
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
